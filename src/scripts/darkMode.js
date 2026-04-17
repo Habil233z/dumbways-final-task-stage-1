5e39cd9f67eaa4b9736c09f4d178d184a3739257
habil <habilherdianto233z@gmail.com>
2026-04-17 16:20:39 +0700

    add transition from dark to light and other way around

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,3 +1,20 @@
+let darkMode = localStorage.getItem("darkMode")
+const themeSwithc = document.getElementById("darkMode")
+
+function enableDarkMode() {
+    document.body.classList.add("darkMode")
+    localStorage.setItem("darkMode", "active")
+}
+
+function disableDarkMode() {
+    document.body.classList.remove("darkMode")
+    document.body.classList.add("lightMode")
+    localStorage.setItem("darkMode", null)
+}
+
+if (darkMode === "active") {enableDarkMode()}
+
 document.getElementById("darkMode").addEventListener("click", function () {
-    document.body.classList.toggle("darkMode")
+    darkMode = localStorage.getItem("darkMode")
+    darkMode !== "active" ? enableDarkMode() : disableDarkMode()
 })

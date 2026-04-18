430ebaa5f9474871afde48950b1b1d869eda90c0
habil <habilherdianto233z@gmail.com>
2026-04-18 19:02:30 +0700

    add hover, reduce js dependencies into just 1 file add small line for content, and animate hover

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,21 @@
+function showDetail(id) {
+    const modal = document.getElementById(`myModal${id}`)
+    modal.classList.toggle("show");
+}
+
+function closeDetail(id) {
+    const modal = document.getElementById(`myModal${id}`)
+    modal.classList.toggle("show");
+}
+
+function closeDetailOutside(id) {
+    const modal = document.getElementById(`myModal${id}`)
+    window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.classList.toggle("show");
+  }
+}}
+
 let darkMode = localStorage.getItem("darkMode")
 const themeSwithc = document.getElementById("darkMode")
 

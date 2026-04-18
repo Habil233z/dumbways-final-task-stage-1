aa293579640dce77279bb3000baf5a13456594f4
habil <habilherdianto233z@gmail.com>
2026-04-18 11:43:11 +0700

    Add detail when pressing my projects card

--- index.js
+++ index.js
@@ -62,10 +62,19 @@ const workExperiences = [
 
 const myProjects = [
   {
+    id: 1,
     image: "/image/my-projects/express-with-database-project.png",
-    name : "Project placeholder",
+    name : "Project Keeper Using DB and Express",
     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nunc risus. Sed blandit gravida diam non vulputate. Curabitur consectetur felis sit amet pretium commodo. Morbi tristique augue ut pretium molestie. Duis quis hendrerit turpis. Curabitur vestibulum leo odio, quis vestibulum nulla sagittis a. Fusce consectetur volutpat turpis, vitae iaculis elit laoreet at. Integer in elementum lectus, sit amet auctor dui. Mauris cursus nisl vitae odio viverra lacinia. Etiam a orci lectus. Mauris nisi nisl, imperdiet id congue sit amet, vehicula eu metus. Mauris bibendum turpis ac mollis tristique.",
     tag : ["Front-end", "Back-end", "Database"],
     githubLink : "https://github.com/Habil233z/personal-web-project-with-express"
+  },
+  {
+    id: 2,
+    image: "/image/my-projects/myPortfolio.png",
+    name : "My Portfolio",
+    description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis nunc risus. Sed blandit gravida diam non vulputate. Curabitur consectetur felis sit amet pretium commodo. Morbi tristique augue ut pretium molestie. Duis quis hendrerit turpis. Curabitur vestibulum leo odio, quis vestibulum nulla sagittis a. Fusce consectetur volutpat turpis, vitae iaculis elit laoreet at. Integer in elementum lectus, sit amet auctor dui. Mauris cursus nisl vitae odio viverra lacinia. Etiam a orci lectus. Mauris nisi nisl, imperdiet id congue sit amet, vehicula eu metus. Mauris bibendum turpis ac mollis tristique.",
+    tag : ["Front-end", "Back-end"],
+    githubLink : ""
   }
 ]
